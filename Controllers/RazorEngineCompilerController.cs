19441647ac647ea71b6d14a7c417db1459b5369e
Nelson Vásquez <nvasquez@aseinfo.com.sv>
2024-05-17 15:48:56 -0600

    feat: adding support to generate html from cshtml templates correctly using RazorLight engine

---
+++ Controllers/RazorEngineCompilerController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RazorLightConsole;
+using System;
+using System.Threading.Tasks;
+
+namespace RazorEngineDockerApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RazorEngineCompilerController : ControllerBase
+    {
+        private readonly IRazorEngineWrapper _razorEngineWrapper;
+
+        public RazorEngineCompilerController(IRazorEngineWrapper razorEngineWrapper)
+        {
+            _razorEngineWrapper = razorEngineWrapper;
+        }
+
+        [HttpGet("string")]
+        public async Task<ActionResult<string>> GetHtmlFromStringRazorTemplateAsync()
+        {
+            string htmlResult;
+            try
+            {
+                htmlResult = await _razorEngineWrapper.GetHtmlFromStringAsync("<h1>Hello, @Model.Name</h1>", new { Name = "Razor Engine" });
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("Error", GetInnerExceptions(ex));
+                return BadRequest(ModelState);
+            }
+
+            return htmlResult;
+        }
+
+        [HttpGet("file")]
+        public async Task<ActionResult<string>> GetHtmlFromFileRazorTemplateAsync()
+        {
+            string htmlResult;
+            try
+            {
+                htmlResult = await _razorEngineWrapper.GetHtmlFromFileAsync("Template.cshtml", new { Name = "Razor Engine" });
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("Error", GetInnerExceptions(ex));
+                return BadRequest(ModelState);
+            }
+
+            return htmlResult;
+        }
+
+        private static string GetInnerExceptions(Exception ex)
+        {
+            string message = ex.Message;
+            if (ex.InnerException != null)
+            {
+                message += " | " + GetInnerExceptions(ex.InnerException);
+            }
+            return message;
+        }
+    }
+}
--- Infrastructure/RazorLightEngineWrapper.cs
+++ Infrastructure/RazorLightEngineWrapper.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using RazorLight;
 
 namespace RazorLightConsole;
@@ -8,22 +9,53 @@ namespace RazorLightConsole;
 public class RazorLightEngineWrapper : IRazorEngineWrapper
 {
     private readonly RazorLightEngine _engine;
+    private readonly ILogger<RazorLightEngineWrapper> _logger;
 
-    public RazorLightEngineWrapper()
+    public RazorLightEngineWrapper(ILogger<RazorLightEngineWrapper> logger)
     {
-        _engine = new RazorLightEngineBuilder()
-                    .UseFileSystemProject(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "RazorTemplates"))
-                    .UseMemoryCachingProvider()
-                    .Build();
+        _logger = logger;
+        _engine = GetConfiguredRazorEngine();
+    }
+
+    private RazorLightEngine GetConfiguredRazorEngine()
+    {
+        try
+        {
+            return new RazorLightEngineBuilder()
+                        .UseFileSystemProject(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "RazorTemplates"))
+                        .UseMemoryCachingProvider()
+                        .Build();
+        }
+        catch (DirectoryNotFoundException ex)
+        {
+            _logger.LogError("Error initializing RazorLightEngine: {Message}", ex.Message);
+            return null;
+        }
     }
 
     public async Task<string> GetHtmlFromFileAsync(string template, object model)
     {
-        return await _engine.CompileRenderAsync(template, model);
+        try
+        {
+            return await _engine.CompileRenderAsync(template, model);
+        }
+        catch (NullReferenceException ex)
+        {
+            _logger.LogError("Error compiling template: {Message}", ex.Message);
+            throw;
+        }
     }
 
     public async Task<string> GetHtmlFromStringAsync(string template, object model)
     {
-        return await _engine.CompileRenderStringAsync("key", template, model);
+        try
+        {
+            return await _engine.CompileRenderStringAsync("key", template, model);
+        }
+        catch (NullReferenceException ex)
+        {
+            _logger.LogError("Error compiling template: {Message}", ex.Message);
+            throw;
+        }
     }
 }
