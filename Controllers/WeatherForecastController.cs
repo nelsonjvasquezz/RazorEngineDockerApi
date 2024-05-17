605486bfe1cb48e61287c0fdf8b913d5fb38b5ed
Nelson Vásquez <nvasquez@aseinfo.com.sv>
2024-05-17 14:45:46 -0600

    style: Update to common api instead of minimal

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RazorEngineDockerApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            var summaries = new[]
+            {
+                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            };
+
+            return string.Join(", ", summaries);
+        }
+    }
+}
