65c060a9486b6a21529c02c3501eb2f8685c4be8
Nelson Vásquez <nvasquez@aseinfo.com.sv>
2024-05-17 17:06:53 -0600

    feat: Agregando nuget Razor.Templating.Core con su respectivo Wrapper. Aún no se ha logrado hacerlo funcionar

---
+++ Infrastructure/RazorTemplatingCoreEngineWrapper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using Razor.Templating.Core;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace RazorEngineDockerApi.Infrastructure
+{
+    public class RazorTemplatingCoreEngineWrapper : IRazorEngineWrapper
+    {
+        private readonly ILogger<RazorTemplatingCoreEngineWrapper> _logger;
+
+        public RazorTemplatingCoreEngineWrapper(ILogger<RazorTemplatingCoreEngineWrapper> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<string> GetHtmlFromFileAsync(string template, object model)
+        {
+            try
+            {
+                return await RazorTemplateEngine.RenderAsync(template, model);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Error compiling template: {Message}", ex.Message);
+                throw;
+            }
+        }
+
+        public Task<string> GetHtmlFromStringAsync(string template, object model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
